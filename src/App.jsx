692b2e9470d4741cf232b02d449c40ff63949858
bianco-zaelot <bianco.emiliozzi@zaelot.com>
2023-10-02 14:04:26 -0300

    fix refreshing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Layout from './components/Layout'
+import { useLocation } from 'react-router-dom'
 
 const LOCAL_IFRAME_URL = 'http://localhost:8080'
 const QA_IFRAME_URL = 'https://test-web-iframe-poc.club-os.dev'
 
+const IFRAME_DOMAIN = QA_IFRAME_URL
+
 export default function App() {
-  const [iframeUrl, setIframeUrl] = useState(LOCAL_IFRAME_URL)
+  const [iframeUrl, setIframeUrl] = useState(IFRAME_DOMAIN)
+  const location = useLocation()
 
   const readIframeUrl = e => {
     if (e.data) {
       window.history.pushState({}, null, e.data)
     }
+  }
+
+  const handleIframeUrl = ({ pathname, search }) => {
+    setIframeUrl(IFRAME_DOMAIN + pathname + search)
   }
 
   useEffect(() => {
@@ -21,14 +29,13 @@ export default function App() {
     }
   }, [])
 
-  const handleIframeUrl = (url) => {
-    const { pathname, search } = new URL(url)
-    setIframeUrl(LOCAL_IFRAME_URL + pathname + search)
-  }
+  useEffect(() => {
+    handleIframeUrl(location)
+  }, [location])
 
   return (
-    <>
-      <Layout iframeUrl={iframeUrl} setIframeUrl={handleIframeUrl} />
-    </>
+    <Layout handleIframeUrl={handleIframeUrl}>
+      <iframe id='app-iframe' src={iframeUrl} />
+    </Layout>
   )
 }
